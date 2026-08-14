efa934277c9a11436a78775c4b24418778c174ad
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2026-08-14 17:56:13 +0700

    fix: add default Supabase URL & Key fallbacks to prevent build environment bundle error

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || '';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || '';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://vlvzoxlwlyvvdxcglgfs.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsdnpveGx3bHl2dmR4Y2dsZ2ZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY2OTIzNDgsImV4cCI6MjEwMjI2ODM0OH0.PQbVbugs0ZzygKLrTYHcMCQ_1XsHQTwbNjnd1mwGGKI';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
